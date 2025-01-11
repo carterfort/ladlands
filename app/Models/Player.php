753ff6b2d134c6691da642dffa794d016cd9b307
Carter Fort <carter.fort@gmail.com>
2025-01-11 10:56:13 -0500

    Add a relationship to the Card class via the location object

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Player extends Model
@@ -22,6 +23,10 @@ class Player extends Model
 
     public function getOpponent(): Player {
         return $this->game->players()->whereNot('id', $this->id)->firstOrFail();
+    }
+
+    public function hand(): HasMany {
+        return $this->hasMany(Card::class, 'location->player_id')->where('location->type', 'player_hand');
     }
 
 }
