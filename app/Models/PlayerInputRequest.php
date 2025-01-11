f7bceaa5e812c11e993ed4065f1975d03b6bb1fd
Carter Fort <carter.fort@gmail.com>
2025-01-11 10:56:34 -0500

    Cast target fields as arrays and add relationships to Game and Player

--- app/Models/PlayerInputRequest.php
+++ app/Models/PlayerInputRequest.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PlayerInputRequest extends Model
 {
     /** @use HasFactory<\Database\Factories\PlayerInputRequestFactory> */
     use HasFactory;
+
+    protected $casts = [
+        'valid_targets' => 'array',
+        'selected_targets' => 'array'
+    ];
+
+    public function game(): BelongsTo {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function owningPlayer(): BelongsTo {
+        return $this->belongsTo(Player::class, 'player_id');
+    }
 }
